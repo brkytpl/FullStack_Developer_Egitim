2c616b15dc97b34a4ccb60cd2a9d28c92cab7a2b
Berkay Topal <brkytpl@gmail.com>
2025-01-12 14:53:09 +0300

    Fullstack derslerimin ilk dersi

---
+++ İlk_ders/Degiskenler/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 12;
+            int y = 5;
+            float z = 6.5f;
+            double t = 11.75;
+            decimal k = 125.56m;
+            char metin = 'E';
+            string ad = "Berkay";
+            bool cevap = false;
+            Console.WriteLine("hello world");
+            Console.WriteLine(x);
+            Console.WriteLine("x den gelen değer:" + x);
+            Console.WriteLine("y değişkeninden gelen değer:{0}", y);
+            Console.WriteLine("Birinci sayı:{0} İkinci sayı:{1}", z, t);
+            Console.WriteLine("birinci sayı:" + z + " İkinci sayı:" + t);
+            Console.WriteLine("Adınız Nedir?");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Girilen isim:" + isim);
+
+            Console.WriteLine("birinci sayıyı giriniz:");
+            Double sayi1 = Convert.ToInt32(Console.ReadLine());//Tür dönüşümü için kullanılır. 
+            Console.WriteLine("ikinci sayıyı giriniz:");
+            Double sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sayi1 + sayi2);
+            double sonuc5 = sayi1 / sayi2;
+            Console.WriteLine(sonuc5);
+
+            Console.WriteLine("sayı giriniz");
+            Double sayı = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("yüzde oranı giriniz");
+            byte yuzde = Convert.ToByte(Console.ReadLine());
+
+            Double sonuc = sayı + (sayı * yuzde / 100);
+            Console.WriteLine(sonuc);
+            decimal sonuc2 = Convert.ToDecimal(sayı + (sayı * yuzde / 100));
+            decimal sonuc3 = (decimal)(sayı + sayı * yuzde / 100);
+            int sonuc4 = (int)(sayı + sayı * yuzde / 100);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ İlk_ders/Degiskenler/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Degiskenler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Degiskenler")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("7c00e26f-21b4-4f69-a0ec-8d3eb116cb45")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
