a0f590cd2ded02498f79d80a0f3f061d31705c27
Berkay Topal <brkytpl@gmail.com>
2025-01-19 16:48:38 +0300

    19 aralık 2.hafta 2.kısım

---
+++ hesaplamalar/hesaplama11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesaplama11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while parantez içi true olana kadar döner
+            //fordan en önemli farkı mantıksal operatör kullanılıyor olmak
+            //1 den 10 a kadar olan sayıların toplamını bulan uygulama
+
+            int i = 1;
+            int toplam = 0;
+            while (i<=10) 
+            { 
+               
+               toplam += i;
+               i++;
+            }
+            while (true) 
+            {
+                i++;
+                if (i == 17) 
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+                
+            }
+            Console.WriteLine("sonuç"+toplam);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ hesaplamalar/hesaplama14_foreach_döngüsü/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesaplama14_foreach_döngüsü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Genellikle listeleme işlemlerinde kullanılır
+            //Array(Diziler):Aynı tipte birden fazla değeri saklamak için kullanılır.
+            //bir diziyi gezmek ve listelemk için foreach kullanılır,dizi ile ilgili işlem yapılacaksa for kullanılır
+
+            int[] sayilar = { 6, 10, 15, 20, 4, 8 };
+            //int[] sayilar2 = new int[5];
+            //sayilar2[0] = 23;
+            //sayilar2[1] = 12;
+            //sayilar2[2] = 13;   
+            //sayilar2[3] = 14;
+            //sayilar2[4] = 15;
+            //Console.WriteLine(sayilar2[4]);
+            foreach (int i in sayilar) 
+            {
+                Console.WriteLine(i);
+            }
+            int[] sayilar2 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("sayı giriniz");
+                sayilar2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int i in sayilar2) 
+            {
+                Console.WriteLine("dizi elemanları:"+i);
+            }
+            string ad = "Berkay";
+            foreach (char i in ad)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
