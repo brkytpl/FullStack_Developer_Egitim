c89dfe1d4f3e28ff578c688941d29097f928a60a
Berkay Topal <brkytpl@gmail.com>
2025-01-26 17:11:36 +0300

    12 adet sorunun çözümü

---
+++ Çalışma soruları/Çalışma Sorusu 11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Çalışma_Sorusu_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Klavyeden 10 rakamını girene kadar kullanıcıdan sayı almaya devam eden ve alınan
+            sayıların ortalamasını hesaplayıp gösteren uygulama*/
+            
+            int girilensayisayisi = 0;
+            double ortalama;
+            double toplam = 0;
+        git:
+            Console.WriteLine("Sayı giriniz");
+            int Sayi= Convert.ToInt32(Console.ReadLine());
+            if (Sayi != 10)
+            {
+                girilensayisayisi++;
+                toplam=toplam+Sayi;
+                ortalama=toplam/girilensayisayisi;
+                Console.WriteLine("Sayıların ortalaması= "+ortalama);
+                goto git;
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ Çalışma soruları/Çalışma sorusu 12/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Çalışma_sorusu_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Random atanan 7 sayıyı bir dizide tutan ve bu sayılardan son rakamı 0 olanların
+            toplamını bulan program*/
+
+            Random random = new Random();
+            int[] sayilar = new int[7];
+            int toplam = 0;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = random.Next(10, 101);
+            }
+
+            Console.WriteLine("Rastgele üretilen sayılar:");
+            foreach (int sayi in sayilar)
+            {
+                Console.Write(sayi + " ");
+                if (sayi % 10 == 0) 
+                {
+                    toplam += sayi;
+                }
+            }
+
+            Console.WriteLine("\nSon Rakamı 0 Olan Sayıların Toplamı: " + toplam);
+
+            Console.ReadKey();
+        }
+    }
+}
