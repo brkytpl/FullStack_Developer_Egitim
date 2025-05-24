560d39b8daaaafb351f5e62e1aaa346227610e0d
Berkay Topal <brkytpl@gmail.com>
2025-05-24 12:10:35 +0300

    MVC konusuna başlandı.

---
+++ web_mvc_dosyalar/razor/razor/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razor.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Urunler()
+        {
+            return View();
+        }
+    }
+}
---
+++ web_mvc_dosyalar/razor/razor/Controllers/HakkimizdeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razor.Controllers
+{
+    public class HakkimizdeController : Controller
+    {
+        // GET: Hakkimizde
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
